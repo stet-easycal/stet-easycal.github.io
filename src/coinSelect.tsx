88f588f126b1bb88546e8ebc7b47031c82dbe5a1
Jinghan Wang <77719492+EnmmmmOvO@users.noreply.github.com>
2025-08-27 18:00:25 +1000

    Update coinSelect.tsx

--- src/coinSelect.tsx
+++ src/coinSelect.tsx
@@ -32,7 +32,7 @@ const CoinSelect : FC<{ setCoin: (arg0: number) => void  }> = ({ setCoin }) => {
 			}}>
 				<Label className="block text-sm/6 font-medium text-gray-900">{t("type")}</Label>
 				<div className="relative mt-2">
-					<ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6 dark:bg-gray-800/50">
+					<ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6">
           <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
             <img
 								alt=""
