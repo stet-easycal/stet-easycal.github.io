94d2513a726306029c8f77725a40b09b78b61f9a
Jinghan Wang <77719492+EnmmmmOvO@users.noreply.github.com>
2025-08-27 17:57:24 +1000

    Update coinSelect.tsx

--- src/coinSelect.tsx
+++ src/coinSelect.tsx
@@ -30,14 +30,14 @@ const CoinSelect : FC<{ setCoin: (arg0: number) => void  }> = ({ setCoin }) => {
 				setSelected(e)
 				setCoin(e.id)
 			}}>
-				<Label className="block text-sm/6 font-medium text-gray-900 dark:text-white">{t("type")}</Label>
+				<Label className="block text-sm/6 font-medium text-gray-900">{t("type")}</Label>
 				<div className="relative mt-2">
-					<ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6 dark:bg-gray-800/50 dark:text-white dark:outline-white/10 dark:focus-visible:outline-indigo-500">
+					<ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6 dark:bg-gray-800/50">
           <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
             <img
 								alt=""
 								src={selected.avatar}
-								className="size-5 shrink-0 bg-gray-100 dark:bg-gray-700 dark:outline dark:-outline-offset-1 dark:outline-white/10"
+								className="size-5 shrink-0 bg-gray-100"
 						/>
             <span className="block truncate">{selected.name}</span>
           </span>
@@ -49,7 +49,7 @@ const CoinSelect : FC<{ setCoin: (arg0: number) => void  }> = ({ setCoin }) => {
 
 					<ListboxOptions
 							transition
-							className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg outline-1 outline-black/5 data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
+							className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg outline-1 outline-black/5 data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm"
 					>
 						{people.map((person) => (
 								<ListboxOption
@@ -57,8 +57,7 @@ const CoinSelect : FC<{ setCoin: (arg0: number) => void  }> = ({ setCoin }) => {
 										value={person}
 										className="group relative cursor-default py-2 pr-9 pl-3 text-gray-900 select-none
 											 data-focus:bg-gradient-to-r data-focus:from-[#1c3fda] data-focus:via-[#0099cc] data-focus:to-[#01c26e]
-											 data-focus:text-white data-focus:outline-hidden
-											 dark:text-white"
+											 data-focus:text-white data-focus:outline-hidden"
 								>
 									<div className="flex items-center">
 										<img
